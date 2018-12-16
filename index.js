e30b2fb7a12aa508a4c681a0600865e0fb546eed
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-16 22:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  let list = document.querySelectorAll('.ranked-list')
+  const list = document.querySelectorAll('.ranked-list')
   for (let i = 0; i < list.length, i++){
     list[i].innerHTML = parseInt(i, n);
   }
